7febeddc7ac8ab30cf7b7bb94a621118a70747a0
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2021-11-14 16:09:41 +0400

    create GreetingGenerator annotation

---
+++ annotation/src/main/java/com/example/annotation/GreetingGenerator.kt
@@ -0,0 +1,11 @@
+package com.example.annotation
+
+/**
+ * Custom Annotation class
+ * For more information check - "https://kotlinlang.org/docs/reference/annotations.html"
+ */
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class GreetingGenerator()
