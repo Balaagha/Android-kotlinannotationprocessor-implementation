b632f4c644415860a37e7f9bb2f202236a864431
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2021-11-14 16:10:37 +0400

    implement our simple annotation processor in main app

--- app/src/main/java/com/example/annotaionprocessorimplementationexample/MainActivity.kt
+++ app/src/main/java/com/example/annotaionprocessorimplementationexample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.annotaionprocessorimplementationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.annotation.GreetingGenerator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    @GreetingGenerator
+    class Santa
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvGreeting.text = Generated_Santa().greeting()
     }
 }
